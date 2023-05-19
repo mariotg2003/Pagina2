275bbd99beb29aa99d5e6afa5c28df5a32073099
mariotg2003 <102721912+mariotg2003@users.noreply.github.com>
2023-05-19 11:50:44 +0200

    cambioJuegoFacil

--- js/logicaFacil.js
+++ js/logicaFacil.js
@@ -2,65 +2,53 @@ let cuerpo=document.getElementById("cuerpo")
 let empezar=document.getElementById("empezar")
 let contador=0
 let puntuacion=document.getElementById("puntuacion")
-puntuacion.innerHTML=7
+puntuacion.innerHTML=5
 let numeros=0
-let areaJuego=document.getElementById("areaJuego")
-
 
 function comprobar(){
     if(contador==0){
     puntuacion.hidden=true
     numeros=0
     puntuacion.innerHTML=""
-    location.replace("https://mariotg2003.github.io/Pagina2/menu.html") 
+    location.replace("https://mariotg2003.github.io/Pagina2/menu.html")
     }  
 }
 
-function generar() {
-  countdown(6);
-  puntuacion.hidden = false;
-  empezar.hidden = true;
-  let numeroBotones = 10;
-  
-  let limite = document.getElementById("areaJuego"); // Elemento límite
-  
-  for (let i = 0; i < numeroBotones; i++) {
-    contador++;
-    let boton = document.createElement("div");
-    boton.classList = "boton";
-    boton.style.position = "absolute";
+function generar(){
+    countdown(7)
+    puntuacion.hidden=false
+    empezar.hidden=true
+    let numeroBotones=8
+    for(let i=0;i<numeroBotones;i++){
+        contador++
+        let boton=document.createElement("div")
+        boton.classList="boton"
+        boton.style.position="absolute"
+        boton.style.left=(Math.random()*(window.innerWidth - 100))+"px"
+        boton.style.top=(Math.random()*(window.innerHeight - 100))+"px"
+        cuerpo.appendChild(boton)
+        boton.addEventListener("click",function(){
+            numeros++
+            boton.remove()
+            contador--
+            comprobar()
+        })      
+        
+    }
     
-    // Obtener dimensiones del elemento límite
-    let limiteRect = limite.getBoundingClientRect();
-    
-    // Calcular posiciones aleatorias dentro del elemento límite
-    let x = Math.random() * (limiteRect.width - 50);
-    let y = Math.random() * (limiteRect.height - 50);
-    
-    boton.style.left = limiteRect.left + x + "px";
-    boton.style.top = limiteRect.top + y + "px";
-    cuerpo.appendChild(boton);
-    
-    boton.addEventListener("click", function() {
-      numeros++;
-      boton.remove();
-      contador--;
-      comprobar();
-    });
-  }
 }
-
 
 function countdown(number) {  
     var countdownInterval = setInterval(function() { 
       puntuacion.innerHTML=number
       number--; 
   
-      if (number < 0 && numeros!=10) {
+      if (number < 0 && numeros!=13) {
         clearInterval(countdownInterval);
         while (cuerpo.lastChild.id!="empezar") {
             cuerpo.removeChild(cuerpo.lastChild);
           }
+        contador=0
         puntuacion.hidden=true
         numeros=0
         empezar.hidden=false
